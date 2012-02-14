c6098db25e8d7366afa5cadf090d035cece80e39
Munaf Assaf <munaf.assaf@gmail.com>
2012-02-14 18:15:29 -0500

    Added comedians JS file

--- data/parse_raw_data.py
+++ data/parse_raw_data.py
@@ -7,9 +7,8 @@ results1 = json.loads(influenced_file.read())['result']
 #results2 = json.loads(influenced_by_file.read())['result']
 
 comedians = {}
-
 for comedian in results1:
-
+	
 	name = comedian['name']
 	comedians[name] = {}
 	comedians[name]['influenced'] = []
@@ -33,4 +32,30 @@ for comedian in results1:
 
 
 
-print json.dumps(comedians)
+output = {}
+
+output['nodes'] = []
+output['links'] = []
+
+for name in comedians:
+	try:
+		src_ind = output['nodes'].index(name)
+	except:
+		output['nodes'].append(unicode(name))
+		src_ind = len(output['nodes']) - 1
+
+	for com in comedians[name]['influenced']:
+		try:
+			dest_ind = output['nodes'].indexc(unicode(com))
+		except:
+			output['nodes'].append(unicode(com))
+			dest_ind = len(output['nodes']) - 1
+	
+		link = {'source':src_ind,'target':dest_ind,'value':1}
+		output['links'].append(link)
+
+out_file = open('comedian_data.js', 'w')
+
+out_file.write(json.dumps(output))
+
+out_file.close()
